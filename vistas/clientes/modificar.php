d02106f2610b05891c9b077e3214bc33a6912b57
lestermaudiel <flestermaudiel2130@gmail.com>
2023-07-24 15:21:48 -0600

    modificacion archivo modificar.php

--- vistas/clientes/modificar.php
+++ vistas/clientes/modificar.php
@@ -1,31 +1,29 @@
 <?php
 require '../../modelos/Cliente.php';
-    try {
-        $cliente = new Cliente($_GET);
-
-        $clientes = $cliente->buscar();
-    
-    } catch (PDOException $e) {
-        $error = $e->getMessage();
-    } catch (Exception $e2){
-        $error = $e2->getMessage();
-    }
+try {
+    $cliente = new Cliente($_GET);
+    $clientes = $cliente->buscar();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
 ?>
-<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/header.php'; ?>
     <div class="container">
         <h1 class="text-center">Modificar Clientes</h1>
         <div class="row justify-content-center">
             <form action="/franco_tarea6/controladores/clientes/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
-                <input type="hidden" name="cliente_id" >
-                    <div class="col">
-                        <label for="cliente_nombre">Nombre del cliente</label>
-                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control" >
-                    </div>
+                <input type="hidden" name="cliente_id">
+                <div class="col">
+                    <label for="cliente_nombre">Nombre del cliente</label>
+                    <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control">
+                </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
                         <label for="cliente_nit">Nit del cliente</label>
-                        <input type="text" step="0.01" min="0" name="cliente_nit" id="cliente_nit" class="form-control" >
+                        <input type="text" step="0.01" min="0" name="cliente_nit" id="cliente_nit" class="form-control">
                     </div>
                 </div>
                 <div class="row mb-3">
@@ -36,4 +34,4 @@ require '../../modelos/Cliente.php';
             </form>
         </div>
     </div>
-<?php include_once '../../includes/footer.php'?>
+<?php include_once '../../includes/footer.php'; ?>
